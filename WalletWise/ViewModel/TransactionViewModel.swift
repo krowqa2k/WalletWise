4cbaf501edecfd2b668aea602994c5c8793a15b2
Mateusz Krówczyński <krafcik226@gmail.com>
2024-08-23 17:38:11 +0200

    add
    added recent transactions

--- WalletWise/ViewModel/TransactionViewModel.swift
+++ WalletWise/ViewModel/TransactionViewModel.swift
@@ -10,10 +10,13 @@ import Foundation
 class TransactionViewModel: ObservableObject {
     
     @Published var transactions: [TransactionModel] = []
+    @Published var recentTransactions: [TransactionModel] = []
     
     func addTransaction(name: String, price: Double, category: TransactionCategory, date: Date) {
         let newTransaction = TransactionModel(name: name, category: category, price: price, date: date)
         transactions.append(newTransaction)
+        recentTransactions.append(newTransaction)
         transactions.sort(by:  { $0.date > $1.date })
+        recentTransactions.sort(by: {$0.date > $1.date })
     }
 }
