702d5c7d529bfce40dff477f4e21fad7623271cf
Mateusz Krówczyński <krafcik226@gmail.com>
2024-08-26 18:32:24 +0200

    feat
    added top spending cell

---
+++ WalletWise/View/Main/Statistics/Components/TopSpendingCell.swift
@@ -0,0 +1,55 @@
+//
+//  TopSpendingCell.swift
+//  WalletWise
+//
+//  Created by Mateusz Krówczyński on 26/08/2024.
+//
+
+import SwiftUI
+
+struct TopSpendingCell: View {
+    var transaction: TransactionModel = .mock
+    private let width: CGFloat = UIScreen.main.bounds.width
+    
+    var body: some View {
+        ZStack {
+            Color.backgroundWW.ignoresSafeArea()
+            
+            RoundedRectangle(cornerRadius: 16)
+                .frame(width: width - 40, height: 80)
+            
+            HStack {
+                ZStack {
+                    Circle()
+                        .frame(width: 60, height: 60)
+                        .foregroundColor(.backgroundWW.opacity(0.1))
+                    Image(systemName: transaction.category.transactionImage)
+                        .font(.title)
+                        .fontWeight(.medium)
+                        .foregroundStyle(Color.random)
+                }
+                
+                VStack(alignment: .leading, spacing: 0){
+                    Text(transaction.name)
+                        .font(.headline)
+                        .foregroundStyle(.backgroundWW)
+                    
+                    Text(transaction.date.displayFormat)
+                        .foregroundStyle(.backgroundWW)
+                }
+                Spacer()
+                HStack(spacing: 0) {
+                    Text("$")
+                    Text(String(format: "%.2f", transaction.price))
+                }
+                .font(.headline)
+                .foregroundStyle(.backgroundWW)
+            }
+            .frame(width: width - 80, height: 80, alignment: .leading)
+        }
+    }
+}
+
+#Preview {
+    TopSpendingCell()
+}
