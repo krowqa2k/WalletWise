d2bd0f6e312414593c483fc41599059de4ef5527
Mateusz Krówczyński <krafcik226@gmail.com>
2024-08-26 18:32:37 +0200

    feat
    added statistic view

--- WalletWise/View/Main/Statistics/View/StatisticsView.swift
+++ WalletWise/View/Main/Statistics/View/StatisticsView.swift
@@ -6,13 +6,65 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 struct StatisticsView: View {
+    
+    @EnvironmentObject private var viewModel: TransactionViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.backgroundWW.ignoresSafeArea()
+            VStack(spacing: 12) {
+                Text("Statistics")
+                    .font(.title3)
+                    .fontWeight(.medium)
+                    .foregroundStyle(.expenseWW)
+                
+                VStack {
+                    Text("$")
+                        .font(.title)
+                        .fontWeight(.medium)
+                        .foregroundStyle(.expenseWW)
+                    +
+                    Text(String(format: "%.2f", viewModel.userExpenses))
+                        .font(.title)
+                        .fontWeight(.medium)
+                        .fontDesign(.rounded)
+                        .foregroundStyle(.expenseWW)
+                    
+                    Text(Date().displayFormat)
+                }
+                .frame(height: 100)
+                .frame(maxWidth: .infinity, alignment: .center)
+                
+                LineChartView()
+                    .frame(height: 300)
+                    .padding(.horizontal)
+                
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 14) {
+                    Text("Top Spendings")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.expenseWW)
+                        .padding(.leading)
+                    
+                    ScrollView(.vertical) {
+                        ForEach(viewModel.transactions.sorted(by: { $0.price > $1.price }).prefix(5)) { transaction in
+                            TopSpendingCell(transaction: transaction)
+                        }
+                    }
+                    .scrollIndicators(.hidden)
+                }
+                
+            }
+        }
     }
 }
 
 #Preview {
     StatisticsView()
+        .environmentObject(TransactionViewModel())
 }
